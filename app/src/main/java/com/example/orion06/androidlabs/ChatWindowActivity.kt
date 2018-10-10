cb67430a8d00abeeb051e6c7acd412c6289eec64
rguzel <32398065+rguzel@users.noreply.github.com>
2018-10-10 16:22:47 -0400

    Finished lab 4

--- app/src/main/java/com/example/orion06/androidlabs/ChatWindowActivity.kt
+++ app/src/main/java/com/example/orion06/androidlabs/ChatWindowActivity.kt
@@ -8,45 +8,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import kotlinx.android.synthetic.main.activity_chat_window.*
-import android.view.KeyEvent.KEYCODE_ENTER
-import android.view.KeyEvent.KEYCODE_DPAD_CENTER
-
 import android.widget.EditText
-
-
 
 class ChatWindowActivity : Activity() {
 
 //Global
-    var myList = null as ListView?
+    private var myList = null as ListView?
     var messages = ArrayList<String>() //automatically grows
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_window)
 
+        val btnSend = findViewById<Button>(R.id.btnDone)
+        val inputText = findViewById<EditText>(R.id.inputText)
 
-        var btnSend = findViewById<Button>(R.id.btnDone)
-        var inputText = findViewById<EditText>(R.id.inputText)
-
-        var messageAdapter = ChatAdapter( this )
-        btnSend?.setOnClickListener( {
-            var userTyped = inputText.getText().toString()
+        val messageAdapter = ChatAdapter( this )
+        btnSend?.setOnClickListener {
+            val userTyped = inputText.getText().toString()
             messages.add(userTyped)
             inputText.setText("")
             messageAdapter.notifyDataSetChanged()
             myList?.setSelection(messageAdapter.getCount() - 1);
-        })
+        }
 
         myList = findViewById<ListView?>(R.id.myList)
         myList?.setAdapter (messageAdapter)
 
-
-
         inputText.setOnKeyListener(object : View.OnKeyListener {
-
             override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
-                if (event.getAction() === KeyEvent.ACTION_DOWN) {
+                val actonId:Int = event.getAction()
+                if ( actonId == KeyEvent.ACTION_DOWN) {
                     when (keyCode) {
                         KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
                             btnSend.callOnClick()
@@ -77,19 +68,15 @@ inner class ChatAdapter(ctx : Context) : ArrayAdapter<String>(ctx, 0 ){
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-
-        var inflater = LayoutInflater.from(parent.getContext())
+        val inflater = LayoutInflater.from(parent.getContext())
         var result = null as View?
-
-        if (position %2 == 0) {
+        if(position %2 == 0) {
             result = inflater.inflate(R.layout.chat_row_incoming, null)
         }
         else {
             result = inflater.inflate(R.layout.chat_row_outgoing, null)
         }
        val message = result?.findViewById<TextView>(R.id.message_text)
-       // val message = result.findViewById(R.id.message_text)
-
         message?.setText( getItem(position) ) // get the string at position
 
         return result
