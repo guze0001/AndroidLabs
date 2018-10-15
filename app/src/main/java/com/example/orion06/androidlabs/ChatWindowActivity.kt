73e97e47072856451f94c90ebe3ceb9d719aaf5d
rguzel <32398065+rguzel@users.noreply.github.com>
2018-10-15 11:39:26 -0400

    Finished lab 5

--- app/src/main/java/com/example/orion06/androidlabs/ChatWindowActivity.kt
+++ app/src/main/java/com/example/orion06/androidlabs/ChatWindowActivity.kt
@@ -1,8 +1,12 @@
 package com.example.orion06.androidlabs
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -12,12 +16,39 @@ import android.widget.EditText
 
 class ChatWindowActivity : Activity() {
 
+    val ACTIVITY_NAME = "ChatWindowActivity"
 //Global
     private var myList = null as ListView?
     var messages = ArrayList<String>() //automatically grows
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_window)
+
+
+        val dhHelper = MyOpenHelper()
+        val db = dhHelper.writableDatabase
+        val results =db.query(TABLE_NAME, arrayOf("_id", KEY_MESSAGES), null, null, null, null, null, null)
+        val numRows = results.count
+        //Row Count
+        Log.i(ACTIVITY_NAME,"Cursor's row count:" + numRows);
+        results.moveToFirst() // point to first row
+        val idIndex = results.getColumnIndex("_id")
+        val messagesIndex = results.getColumnIndex(KEY_MESSAGES)
+        while(!results.isAfterLast())
+        {
+            var thisID = results.getInt(idIndex)
+            val msg = results.getString(messagesIndex)
+            messages.add(msg)
+            print("FirstName $msg")
+            Log.i(ACTIVITY_NAME,"SQL MESSAGE: $msg ")
+            results.moveToNext()
+        }
+        val numOfColumns = results.columnCount
+        for (i  in 0..numOfColumns-1)//in (numOfColumns -1).downTo(0) )
+        {
+            Log.i(ACTIVITY_NAME, "Column Name : "+ results.getColumnName(i))
+        }
+
 
         val btnSend = findViewById<Button>(R.id.btnDone)
         val inputText = findViewById<EditText>(R.id.inputText)
@@ -26,6 +57,13 @@ class ChatWindowActivity : Activity() {
         btnSend?.setOnClickListener {
             val userTyped = inputText.getText().toString()
             messages.add(userTyped)
+
+            //write to the db
+            val row = ContentValues()
+            row.put(KEY_MESSAGES,userTyped)
+            db.insert(TABLE_NAME, null, row)
+
+
             inputText.setText("")
             messageAdapter.notifyDataSetChanged()
             myList?.setSelection(messageAdapter.getCount() - 1);
@@ -85,6 +123,24 @@ inner class ChatAdapter(ctx : Context) : ArrayAdapter<String>(ctx, 0 ){
     override fun getItemId(position: Int): Long {
         return super.getItemId(position)
         return position.toLong()
+    }
+}
+
+    val DATABASE_NAME = "MyDatabaseFile.db"
+    val VERSION_NUM = 1
+    val TABLE_NAME = "Messages"
+    val KEY_MESSAGES = "Messages"
+
+inner class MyOpenHelper:SQLiteOpenHelper(this@ChatWindowActivity, DATABASE_NAME, null, VERSION_NUM){
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE $TABLE_NAME ( _id INTEGER PRIMARY KEY AUTOINCREMENT, $KEY_MESSAGES TEXT);")
+
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        db.execSQL("DROP TABLE IF EXISTS$TABLE_NAME")
+        onCreate(db)
     }
 }
 
